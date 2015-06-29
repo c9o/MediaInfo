451160aff7d3e21e3d35f6370737b5c061463436
Jingtao Chen <Jingtao.Chen@csr.com>
2015-06-29 15:48:16 +0800

    Revert "Revert "Add \0 at last""
    This reverts commit 7fcc4eb8ef00e8896b808a34fb46e344a2e735e7.

--- lib/printinfo.c
+++ lib/printinfo.c
@@ -86,4 +86,5 @@ void print_info (GstDiscovererInfo * info, GError * err, gchar message[])
 
 	offset += snprintf (msg+offset, sizeof(msg)-offset, "\n");
 	strncpy(message, msg, offset);
+	message[offset] = '\0';
 }
