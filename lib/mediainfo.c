3fc069f7d354388e6802884229a390bb27d4984d
Jingtao Chen <Jingtao.Chen@csr.com>
2015-07-22 11:15:27 +0800

    Metadata
    Signed-off-by: Jingtao Chen <Jingtao.Chen@csr.com>

--- lib/mediainfo.c
+++ lib/mediainfo.c
@@ -13,28 +13,49 @@ static void print_fps(double d, const char *postfix)
 	if (v % 100)
 	{
 		offset += snprintf (msg+offset, sizeof(msg)-offset, ", %3.2f %s", d, postfix);
+		if (strcmp(postfix, "fps") == 0)
+			snprintf (mMetadataValues[MetaNameMap[5].key], MAX_METADATA_STRING_LENGTH, "%3.2f %s", d, postfix);
+		else
+			snprintf (mMetadataValues[MetaNameMap[7].key], MAX_METADATA_STRING_LENGTH, "%3.2f %s", d, postfix);
 	}
 	else if (v % (100 * 1000))
 	{
 		offset += snprintf (msg+offset, sizeof(msg)-offset, ", %1.0f %s", d, postfix);
+		if (strcmp(postfix, "fps") == 0)
+			snprintf (mMetadataValues[MetaNameMap[5].key], MAX_METADATA_STRING_LENGTH, "%1.0f %s", d, postfix);
+		else
+			snprintf (mMetadataValues[MetaNameMap[7].key], MAX_METADATA_STRING_LENGTH, "%1.0f %s", d, postfix);
 	}
 	else
 	{
 		offset += snprintf (msg+offset, sizeof(msg)-offset, ", %1.0fk %s", d / 1000, postfix);
+		snprintf (mMetadataValues[MetaNameMap[6].key], MAX_METADATA_STRING_LENGTH, "%1.0fk %s", d / 1000, postfix);
 	}
 }
 
 static void dump_metadata(void *ctx, AVDictionary *m, const char *indent)
 {
+	int i = 0;
 	if (m && !(av_dict_count(m) == 1 && av_dict_get(m, "language", NULL, 0))) {
 		AVDictionaryEntry *tag = NULL;
 
 		offset += snprintf (msg+offset, sizeof(msg)-offset, "%sMetadata:\n", indent);
+		#if 1
 		while ((tag = av_dict_get(m, "", tag, AV_DICT_IGNORE_SUFFIX)))
 			if (strcmp("language", tag->key))
 			{
 				offset += snprintf (msg+offset, sizeof(msg)-offset, "%s  %-8s: %s\n", indent, tag->key, tag->value);
 			}
+		#endif
+
+		for (i = 12; i < (int) DIM (MetaNameMap); i++) {
+		tag = av_dict_get (m, MetaNameMap[i].tagName, NULL, AV_DICT_IGNORE_SUFFIX);
+
+		if (tag) {
+			snprintf (mMetadataValues[MetaNameMap[i].key], MAX_METADATA_STRING_LENGTH, "%s", tag->value);
+			offset += snprintf (msg+offset, sizeof(msg)-offset, "%s  %-8s: %s\n", indent, tag->key, tag->value);
+		}
+		}
 	}
 
 }
@@ -42,12 +63,10 @@ static void dump_metadata(void *ctx, AVDictionary *m, const char *indent)
 /* "user interface" functions */
 static void dump_stream_format(AVFormatContext *ic, int i, int index, int is_output)
 {
-	char buf[256];
 	int flags = (is_output ? ic->oformat->flags : ic->iformat->flags);
 	AVStream *st = ic->streams[i];
 	AVDictionaryEntry *lang = av_dict_get(st->metadata, "language", NULL, 0);
-	avcodec_string(buf, sizeof(buf), st->codec, is_output);
-	offset += snprintf (msg+offset, sizeof(msg)-offset, "Stream #%d.%d", index, i);
+
 	/* the pid is an important information, so we display it */
 	/* XXX: add a generic system */
 	if (flags & AVFMT_SHOW_IDS)
@@ -57,8 +76,9 @@ static void dump_stream_format(AVFormatContext *ic, int i, int index, int is_out
 	if (lang)
 	{
 		offset += snprintf (msg+offset, sizeof(msg)-offset, "(%s)", lang->value);
+		snprintf (mMetadataValues[MetaNameMap[17].key], MAX_METADATA_STRING_LENGTH, "(%s)", lang->value);
 	}
-	offset += snprintf (msg+offset, sizeof(msg)-offset, ": %s", buf);
+
 	if (st->sample_aspect_ratio.num && // default
 			av_cmp_q(st->sample_aspect_ratio, st->codec->sample_aspect_ratio)) {
 		AVRational display_aspect_ratio;
@@ -67,6 +87,8 @@ static void dump_stream_format(AVFormatContext *ic, int i, int index, int is_out
 				st->codec->height * st->sample_aspect_ratio.den,
 				1024 * 1024);
 		offset += snprintf (msg+offset, sizeof(msg)-offset, ", PAR %d:%d DAR %d:%d", st->sample_aspect_ratio.num, st->sample_aspect_ratio.den, display_aspect_ratio.num, display_aspect_ratio.den);
+		snprintf (mMetadataValues[MetaNameMap[8].key], MAX_METADATA_STRING_LENGTH, "PAR %d:%d", st->sample_aspect_ratio.num, st->sample_aspect_ratio.den);
+		snprintf (mMetadataValues[MetaNameMap[9].key], MAX_METADATA_STRING_LENGTH, "DAR %d:%d", display_aspect_ratio.num, display_aspect_ratio.den);
 	}
 	if (st->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
 		if (st->avg_frame_rate.den && st->avg_frame_rate.num)
@@ -77,7 +99,7 @@ static void dump_stream_format(AVFormatContext *ic, int i, int index, int is_out
 			print_fps(1 / av_q2d(st->codec->time_base), "tbc");
 	}
 	offset += snprintf (msg+offset, sizeof(msg)-offset, "\n");
-	dump_metadata(NULL, st->metadata, "");
+	//dump_metadata(NULL, st->metadata, "");
 }
 
 void dump_format(AVFormatContext *ic, int index, const char *url, int is_output)
@@ -101,30 +123,62 @@ void dump_format(AVFormatContext *ic, int index, const char *url, int is_output)
 			hours = mins / 60;
 			mins %= 60;
 			offset += snprintf (msg+offset, sizeof(msg)-offset, "%02d:%02d:%02d.%02d", hours, mins, secs, (100 * us) / AV_TIME_BASE);
+			snprintf (mMetadataValues[MetaNameMap[0].key], MAX_METADATA_STRING_LENGTH, "%02d:%02d:%02d.%02d", hours, mins, secs, (100 * us) / AV_TIME_BASE);
 		} else {
 			offset += snprintf (msg+offset, sizeof(msg)-offset, "N/A");
+			snprintf (mMetadataValues[MetaNameMap[0].key], MAX_METADATA_STRING_LENGTH, "N/A");
 		}
 		if (ic->start_time != AV_NOPTS_VALUE) {
-			int secs, us;
+			int hours, mins, secs, us;
 			offset += snprintf (msg+offset, sizeof(msg)-offset, ", start: ");
 			secs = ic->start_time / AV_TIME_BASE;
 			us   = abs(ic->start_time % AV_TIME_BASE);
-			offset += snprintf (msg+offset, sizeof(msg)-offset, "%d.%06d", secs, (int) av_rescale(us, 1000000, AV_TIME_BASE));
+			mins  = secs / 60;
+			secs %= 60;
+			hours = mins / 60;
+			mins %= 60;
+			offset += snprintf (msg+offset, sizeof(msg)-offset, "%02d:%02d:%02d.%06d", hours, mins, secs, (int) av_rescale(us, 1000000, AV_TIME_BASE));
+			snprintf (mMetadataValues[MetaNameMap[1].key], MAX_METADATA_STRING_LENGTH, "%02d:%02d:%02d.%06d", hours, mins, secs, (int) av_rescale(us, 1000000, AV_TIME_BASE));
 		}
 		offset += snprintf (msg+offset, sizeof(msg)-offset, ", bitrate: ");
 		if (ic->bit_rate)
 		{
 			offset += snprintf (msg+offset, sizeof(msg)-offset, "%d kb/s", ic->bit_rate / 1000);
+			snprintf (mMetadataValues[MetaNameMap[2].key], MAX_METADATA_STRING_LENGTH, "%d kb/s", ic->bit_rate / 1000);
 		}
 		else
 		{
 			offset += snprintf (msg+offset, sizeof(msg)-offset, "N/A");
+			snprintf (mMetadataValues[MetaNameMap[2].key], MAX_METADATA_STRING_LENGTH, "N/A");
 		}
 		offset += snprintf (msg+offset, sizeof(msg)-offset, "\n");
 	}
+
+	
 	for (i = 0; i < ic->nb_streams; i++)
+	{
+		AVStream *st = ic->streams[i];
+    	AVCodecContext *codec = st->codec;
+    	AVCodec *decoder = avcodec_find_decoder (codec->codec_id);
+    	int codec_type = codec->codec_type;
+    	char buf[256];
+
+    	avcodec_string(buf, sizeof(buf), st->codec, is_output);
+
+    	offset += snprintf (msg+offset, sizeof(msg)-offset, "Stream #%d.%d", index, i);
+    	offset += snprintf (msg+offset, sizeof(msg)-offset, ": %s", buf);
+
+    	if (codec_type == AVMEDIA_TYPE_VIDEO) {
+      		snprintf (mMetadataValues[MetaNameMap[4].key], MAX_METADATA_STRING_LENGTH, "true");
+      		snprintf (mMetadataValues[MetaNameMap[10].key], MAX_METADATA_STRING_LENGTH, "%s", buf);
+    	} else if (codec_type == AVMEDIA_TYPE_AUDIO) {
+      		snprintf (mMetadataValues[MetaNameMap[3].key], MAX_METADATA_STRING_LENGTH, "true");
+      		snprintf (mMetadataValues[MetaNameMap[11].key], MAX_METADATA_STRING_LENGTH, "%s", buf);
+    	}
+
 		if (!printed[i])
 			dump_stream_format(ic, i, index, is_output);
+	}
 
 	av_free(printed);
 }
@@ -163,6 +217,7 @@ int openFile (const char *url/*, AVFormatContext *format*/)
 void media_info (const char *filename, char message[])
 {
 	printf ("filename is %s\n", filename);
+	int i = 0;
 
 	/* init libav */
 	av_register_all ();
@@ -170,7 +225,22 @@ void media_info (const char *filename, char message[])
 
 	offset = 0;
 
+	for (i = 0; i < (int) DIM (MetaNameMap); i++) {
+		snprintf (mMetadataValues[MetaNameMap[i].key], MAX_METADATA_STRING_LENGTH, "N/A");
+	} 
+	
 	openFile (filename);
+
+	for (i = 0; i < (int) DIM (MetaNameMap); i++) {
+		//tag = av_dict_get (mFormat->metadata, MetaNameMap[i].tagName, NULL, AV_DICT_IGNORE_SUFFIX);
+
+/*		if (tag) {
+			snprintf (mMetadataValues[MetaNameMap[i].key], MAX_METADATA_STRING_LENGTH, "%s", tag->value);
+		} */
+
+	printf ("%d %s \r\n", MetaNameMap[i].key, mMetadataValues[MetaNameMap[i].key]);
+
+	} 
 
 	offset += snprintf (msg+offset, sizeof(msg)-offset, "\n");
 	strncpy(message, msg, offset);
--- lib/mediainfo.h
+++ lib/mediainfo.h
@@ -1,104 +1,73 @@
 #ifndef __MEDIAINFO_H__
 #define __MEDIAINFO_H__
 #include <stdio.h>
-#if 0
-/** album        -- name of the set this work belongs to
- * album_artist -- main creator of the set/album, if different from artist.
- *                 e.g. "Various Artists" for compilation albums.
- * artist       -- main creator of the work
- * comment      -- any additional description of the file.
- * composer     -- who composed the work, if different from artist.
- * copyright    -- name of copyright holder.
- * date         -- date when the work was created, preferably in ISO 8601.
- * disc         -- number of a subset, e.g. disc in a multi-disc collection.
- * encoder      -- name/settings of the software/hardware that produced the file.
- * encoded_by   -- person/group who created the file.
- * filename     -- original name of the file.
- * genre        -- <self-evident>.
- * language     -- main language in which the work is performed, preferably
- *                 in ISO 639-2 format.
- * performer    -- artist who performed the work, if different from artist.
- *                 E.g for "Also sprach Zarathustra", artist would be "Richard
- *                 Strauss" and performer "London Philharmonic Orchestra".
- * publisher    -- name of the label/publisher.
- * title        -- name of the work.
- * track        -- number of this work in the set, can be in form current/total.
- */
+
+#if 1
+
 typedef struct MetaNameMapEntry
 {
 	int key;
 	const char *tagName;
 } MetaNameMapEntry;
 
-// Keep these in synch with the constants defined in MediaMetadataRetriever.java
 enum {
-	METADATA_KEY_CD_TRACK_NUMBER 		= 0,
-	METADATA_KEY_ALBUM           		= 1,
-	METADATA_KEY_ARTIST          		= 2,
-	METADATA_KEY_AUTHOR          		= 3,
-	METADATA_KEY_COMPOSER        		= 4,
-	METADATA_KEY_DATE            		= 5,
-	METADATA_KEY_GENRE           		= 6,
-	METADATA_KEY_TITLE           		= 7,
-	METADATA_KEY_YEAR            		= 8,
-	METADATA_KEY_DURATION        		= 9,
-	METADATA_KEY_NUM_TRACKS      		= 10,
-	METADATA_KEY_WRITER          		= 11,
-	METADATA_KEY_MIMETYPE        		= 12,
-	METADATA_KEY_ALBUMARTIST     		= 13,
-	METADATA_KEY_DISC_NUMBER     		= 14,
-	METADATA_KEY_COMPILATION     		= 15,
-	METADATA_KEY_HAS_AUDIO       		= 16,
-	METADATA_KEY_HAS_VIDEO       		= 17,
-	METADATA_KEY_VIDEO_WIDTH     		= 18,
-	METADATA_KEY_VIDEO_HEIGHT    		= 19,
-	METADATA_KEY_BITRATE         		= 20,
-	METADATA_KEY_TIMED_TEXT_LANGUAGES 	= 21,
-	METADATA_KEY_IS_DRM          		= 22,
-	METADATA_KEY_LOCATION        		= 23,
-	METADATA_KEY_VIDEO_ROTATION  		= 24,
-	METADATA_KEY_CODEC 			 		= 25,
-	METADATA_KEY_RATING			 		= 26,
-	METADATA_KEY_COMMENT		 		= 27,
-	METADATA_KEY_COPYRIGHT		 		= 28,
-	METADATA_KEY_FRAME_RATE		 		= 29,
-	METADATA_KEY_VIDEO_FORMAT	 		= 30,
+	METADATA_KEY_DURATION				= 0,
+	METADATA_KEY_START_TIME				= 1,
+	METADATA_KEY_BITRATE				= 2,
+	METADATA_KEY_HAS_AUDIO				= 3,
+	METADATA_KEY_HAS_VIDEO				= 4,
+	METADATA_KEY_FRAME_RATE				= 5,
+	METADATA_KEY_TBN					= 6,
+	METADATA_KEY_TBC					= 7,
+	METADATA_KEY_PAR					= 8,
+	METADATA_KEY_DAR					= 9,
+	METADATA_KEY_VIDEO_CODEC			= 10,
+	METADATA_KEY_AUDIO_CODEC			= 11,
+	METADATA_KEY_TITLE					= 12,
+	METADATA_KEY_ALBUM					= 13,
+	METADATA_KEY_ARTIST					= 14,
+	METADATA_KEY_GENRE					= 15,
+	METADATA_KEY_ALBUMARTIST			= 16,
+	METADATA_KEY_LANGUAGE				= 17,
+	METADATA_KEY_CD_TRACK_NUMBER		= 18,
+	METADATA_KEY_DATE					= 19,
+	METADATA_KEY_TYER					= 20,
+	METADATA_KEY_COMMENT				= 21,
+	METADATA_KEY_TAGGING_TIME			= 22,
+	METADATA_KEY_TOOL_NAME				= 23,
+	METADATA_KEY_ENCODER				= 24,
+	METADATA_KEY_YEAR					= 25,
 
 	// Add more here...
 };
 
 MetaNameMapEntry MetaNameMap[] = {
-	{METADATA_KEY_CD_TRACK_NUMBER, 		"track"},
-	{METADATA_KEY_ALBUM, 				"album"},
-	{METADATA_KEY_ARTIST, 				"artist"},
-	{METADATA_KEY_AUTHOR, 				"author"},
-	{METADATA_KEY_COMPOSER, 			"composer"},
-	{METADATA_KEY_DATE, 				"date"},
-	{METADATA_KEY_GENRE, 				"genre"},
-	{METADATA_KEY_TITLE, 				"title"},
-	{METADATA_KEY_YEAR, 				"year"},
-	{METADATA_KEY_DURATION, 			"duration"},
-	{METADATA_KEY_NUM_TRACKS, 			"track count"},
-	{METADATA_KEY_WRITER, 				"writer"},
-	{METADATA_KEY_MIMETYPE, 			"mime type"},
-	{METADATA_KEY_ALBUMARTIST, 			"album artist"},
-	{METADATA_KEY_DISC_NUMBER, 			"disc"},
-	{METADATA_KEY_COMPILATION, 			"compilation"},
-	{METADATA_KEY_HAS_AUDIO, 			"has audio"},
-	{METADATA_KEY_HAS_VIDEO, 			"has video"},
-	{METADATA_KEY_VIDEO_WIDTH, 			"width"},
-	{METADATA_KEY_VIDEO_HEIGHT, 		"height"},
-	{METADATA_KEY_BITRATE,   			"bitrate"},
-	{METADATA_KEY_TIMED_TEXT_LANGUAGES,	"language"},
-	{METADATA_KEY_IS_DRM, 				"drm"},
-	{METADATA_KEY_LOCATION, 			"location"},
-	{METADATA_KEY_VIDEO_ROTATION, 		"rotation"},
-	{METADATA_KEY_CODEC, 				"codec"},
-	{METADATA_KEY_RATING,     			"rating"},
-	{METADATA_KEY_COMMENT, 				"comment"},
-	{METADATA_KEY_COPYRIGHT, 			"copyright"},
-	{METADATA_KEY_FRAME_RATE, 			"framerate"},
-	{METADATA_KEY_VIDEO_FORMAT, 		"videoformat"},
+	{METADATA_KEY_DURATION,				"duration"},
+	{METADATA_KEY_START_TIME,			"start time"},
+	{METADATA_KEY_BITRATE,				"bitrate"},
+	{METADATA_KEY_HAS_AUDIO,			"has audio"},
+	{METADATA_KEY_HAS_VIDEO,			"has video"},
+	{METADATA_KEY_FRAME_RATE,			"framerate"},
+	{METADATA_KEY_TBN,					"tbn"},
+	{METADATA_KEY_TBC,					"tbc"},
+	{METADATA_KEY_PAR,					"PAR"},
+	{METADATA_KEY_DAR,					"DAR"},
+	{METADATA_KEY_VIDEO_CODEC,			"video codec"},
+	{METADATA_KEY_AUDIO_CODEC,			"audio codec"},
+	{METADATA_KEY_TITLE,				"title"},
+	{METADATA_KEY_ALBUM,				"album"},
+	{METADATA_KEY_ARTIST,				"artist"},
+	{METADATA_KEY_GENRE,				"genre"},
+	{METADATA_KEY_ALBUMARTIST,			"albumartist"},
+	{METADATA_KEY_LANGUAGE,				"language"},
+	{METADATA_KEY_CD_TRACK_NUMBER,		"track"},
+	{METADATA_KEY_DATE,					"date"},
+	{METADATA_KEY_TYER,					"tyer"},
+	{METADATA_KEY_COMMENT,				"comment"},
+	{METADATA_KEY_TAGGING_TIME,			"tagging time"},
+	{METADATA_KEY_TOOL_NAME,			"tool name"},
+	{METADATA_KEY_ENCODER,				"encoder"},
+	{METADATA_KEY_YEAR,					"year"},
 };
 
 typedef struct MimeMapEntry
@@ -119,10 +88,10 @@ MimeMapEntry MimeMap[] = {
 };
 
 const int MAX_METADATA_STRING_LENGTH = 128;
-const int NUM_METADATA_KEYS = 16;
+const int NUM_METADATA_KEYS = 26;
 
 #define DIM(a) (sizeof(a)/ sizeof(a[0]))
-//char mMetadataValues[NUM_METADATA_KEYS][MAX_METADATA_STRING_LENGTH];
+char mMetadataValues[26][128];
 #endif
 
 void media_info (const char * filename, char msg[]);
